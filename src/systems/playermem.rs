047abc2c220bd60cc675b30d2e7e7327bcc43ca6
Cole Heslinga <cole.heslinga@gmail.com>
2021-03-10 15:44:20 -0500

    Day 5: Fixed player memory still being visible upon revisiting the memorized spot

--- src/systems/playermem.rs
+++ src/systems/playermem.rs
@@ -10,11 +10,19 @@ pub fn update_player_memory(objects: &mut Vec<Object>) {
         };
 
     for obj in objects.iter_mut() {
-        if let Some(pos) = &obj.pos {
+        if let Object { pos: Some(pos), player_mem: mem, .. } = obj {
+            //Clear out the player memory if that spot has been seen again
+            if mem.last_pos.is_some() {
+                if visible.contains(mem.last_pos.as_ref().unwrap()) {
+                    mem.seen = false;
+                    mem.last_pos = None;
+                }
+            }
+
             'inner: for p in visible.iter() {
                 if p == pos {
-                    obj.player_mem.seen = true;
-                    obj.player_mem.last_pos = Some(*p);
+                    mem.seen = true;
+                    mem.last_pos = Some(*p);
                     break 'inner;
                 }
             }
