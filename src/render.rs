ffb814437f062f565bb11f846f3e8d88eb9e0aa1
Cole Heslinga <cole.heslinga@gmail.com>
2021-03-10 13:20:45 -0500

    Day 5: Started working on UI functionality

--- src/render.rs
+++ src/render.rs
@@ -10,7 +10,7 @@ pub fn render_loop(gs: &State, con: &mut BTerm) {
         ContextState::InGame => {
             batch_map_draws(&gs.world.map, &gs.world.camera);
             batch_entity_draws(&gs.world.objects, &gs.world.map, &gs.world.camera, gs.world.depth);
-            batch_ui_draws(&gs.logs);
+            batch_ui_draws(&gs.world.objects, &gs.logs);
         }
     }
     render_draw_buffer(con).expect("Failed to render");
@@ -86,27 +86,68 @@ fn batch_entity_draws(objects: &Vec<Object>, map: &Map, camera: &Camera, floor: 
     batch.submit(5000).expect("Failed to batch entity draw");
 }
 
-fn batch_ui_draws(logs: &LogBuffer) {
+fn batch_ui_draws(objects: &Vec<Object>, logs: &LogBuffer) {
     let mut bg_batch = DrawBatch::new();
     let mut txt_batch = DrawBatch::new();
     bg_batch.target(MAP_CON);
     txt_batch.target(TEXT_CON);
 
+    //UI Box definitions
     let ui_box: Rect = Rect::with_size(CONSOLE_W + 1 - UI_CUTOFF.x, 0, UI_CUTOFF.x - 2, CONSOLE_H - 1);
     let log_box: Rect = Rect::with_size(0, CONSOLE_H - UI_CUTOFF.y, CONSOLE_W - UI_CUTOFF.x, UI_CUTOFF.y - 1);
 
+    //Draw both UI boxes
     bg_batch.draw_double_box(ui_box, ColorPair::new(GREY50, BLACK));
     bg_batch.draw_double_box(log_box, ColorPair::new(GREY50, BLACK));
 
-    txt_batch.fill_region(Rect::with_size(ui_box.x1 * 2 + 2, 1, (ui_box.width() * 2) - 3, 0), ColorPair::new(LIME_GREEN,BLACK), 219);
-    txt_batch.print_color(Point::new(ui_box.x1 * 2 + 4, 1), "Party", ColorPair::new(BLACK, LIME_GREEN));
+    //Print headers for each section of the UI box
+    txt_batch.fill_region(Rect::with_size(ui_box.x1 * 2 + 2, 1, (ui_box.width() * 2) - 3, 0),
+                          ColorPair::new(LIME_GREEN,BLACK), 219);
+    txt_batch.print_color(Point::new(ui_box.x1 * 2 + 4, 1),
+                          "Party", ColorPair::new(BLACK, LIME_GREEN));
 
-    txt_batch.fill_region(Rect::with_size(ui_box.x1 * 2 + 2, 21, (ui_box.width() * 2) - 3, 0), ColorPair::new(RED,BLACK), 219);
-    txt_batch.print_color(Point::new(ui_box.x1 * 2 + 4, 21), "Combat", ColorPair::new(BLACK, RED));
+    txt_batch.fill_region(Rect::with_size(ui_box.x1 * 2 + 2, 21, (ui_box.width() * 2) - 3, 0),
+                          ColorPair::new(RED,BLACK), 219);
+    txt_batch.print_color(Point::new(ui_box.x1 * 2 + 4, 21),
+                          "Combat", ColorPair::new(BLACK, RED));
 
-    txt_batch.fill_region(Rect::with_size(ui_box.x1 * 2 + 2, 41, (ui_box.width() * 2) - 3, 0), ColorPair::new(GOLD,BLACK), 219);
-    txt_batch.print_color(Point::new(ui_box.x1 * 2 + 4, 41), "Abilities", ColorPair::new(BLACK, GOLD));
+    txt_batch.fill_region(Rect::with_size(ui_box.x1 * 2 + 2, 41, (ui_box.width() * 2) - 3, 0),
+                          ColorPair::new(GOLD,BLACK), 219);
+    txt_batch.print_color(Point::new(ui_box.x1 * 2 + 4, 41),
+                          "Abilities", ColorPair::new(BLACK, GOLD));
 
+    //Create the sub-boxes for party members, abilities, etc.
+    let party_box = Rect::with_size(ui_box.x1 * 2 + 2, 2, (ui_box.width() * 2) - 3, 18);
+    let combat_box = Rect::with_size(ui_box.x1 * 2 + 2, 22, (ui_box.width() * 2) - 3, 18);
+    let ability_box = Rect::with_size(ui_box.x1 * 2 + 2, 42, (ui_box.width() * 2) - 3, 16);
+
+    /*
+    //Region fills for debugging purposes
+    txt_batch.fill_region(party_box, ColorPair::new(DARK_GREEN, BLACK), 219);
+    txt_batch.fill_region(combat_box, ColorPair::new(DARK_RED, BLACK), 219);
+    txt_batch.fill_region(ability_box, ColorPair::new(DARK_GOLDENROD, BLACK), 219);
+    */
+
+    let party_sub_boxes = get_divider_boxes(&party_box);
+    let combat_sub_boxes = get_divider_boxes(&combat_box);
+
+    {
+        let player_party = &objects[0].members;
+
+        for (i, member) in player_party.iter().enumerate() {
+            let sbox = party_sub_boxes[i];
+            txt_batch.print_color(Point::new(sbox.x1, sbox.y1), format!("{}", member.name), ColorPair::new(member.icon.get_render().1.fg, BLACK));
+            txt_batch.print_color(Point::new(sbox.x1, sbox.y1 + 1), format!("{}", to_char(member.icon.get_render().0 as u8)), ColorPair::new(member.icon.get_render().1.fg, BLACK));
+        }
+    }
+    for sbox in combat_sub_boxes.iter() {
+        /*
+        txt_batch.fill_region(*sbox, ColorPair::new(WHITE, BLACK), 7);
+        txt_batch.print_color(Point::new(sbox.x1, sbox.y1), format!("Member {}", i), ColorPair::new(BLACK, WHITE));
+         */
+    }
+
+    //Print out the log buffer
     let mut tb = TextBlock::new(LOG_BOX.x1 * 2, LOG_BOX.y1, LOG_BOX.width() * 2, LOG_BOX.height());
     tb.print(&logs.format());
     tb.render_to_draw_batch(&mut txt_batch);
@@ -121,4 +162,18 @@ fn get_tile_render(tile: &TileClass) -> (FontCharType, ColorPair) {
         TileClass::Tree => (5, ColorPair::new(GREEN,BLACK)),
         TileClass::ForestPortal => (21, ColorPair::new(CYAN,BLACK)),
     }
+}
+
+fn get_divider_boxes(source_rect: &Rect) -> Vec<Rect> {
+    let mut boxes = Vec::new();
+    let width_increment = source_rect.width()/4;
+    let widths = vec![0, width_increment, width_increment * 2, width_increment * 3];
+
+    for w in widths.iter() {
+        boxes.push(Rect::with_size(source_rect.x1 + w, source_rect.y1, source_rect.width() / 4, 6));
+        boxes.push(Rect::with_size(source_rect.x1 + w, source_rect.y1 + 6, source_rect.width() / 4, 6));
+        boxes.push(Rect::with_size(source_rect.x1 + w, source_rect.y1 + 12, source_rect.width() / 4, 5));
+    }
+
+    return boxes
 }
